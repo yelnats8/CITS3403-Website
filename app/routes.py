cbb61f5d2a2ffcf6cc9a2d3605528064ece7f661
Yves Reyes <yvesreyes@Yvess-MacBook-Air.local>
2023-05-01 13:18:24 +0800

    Logout's properly implemented

--- app/routes.py
+++ app/routes.py
@@ -2,18 +2,23 @@ from flask import render_template, flash, redirect, url_for
 from app import app, db
 from app.forms import LoginForm, RegisterForm
 from app.models import User
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+@app.route('/home')
+def home():
+    return render_template('base.html', title = 'Home')
 
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -21,16 +26,19 @@ def login():
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index')) 
+        return redirect(url_for('home')) 
     return render_template('login.html', title='Log In', form=form)
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegisterForm()
     if form.validate_on_submit():
         user = User(username = form.username.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
+        flash('Account created')
         return redirect(url_for('login'))
     return render_template('register.html', title ='Register', form=form)
