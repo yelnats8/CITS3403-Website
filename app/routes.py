56776887190568979b10a199807a8aa5a2e50a27
RatKing2002 <nikethkarlapudi75@gmail.com>
2023-05-08 15:24:17 +0800

    Added last seen to profile

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from app import app, db
 from app.forms import LoginForm, RegisterForm, ResetPassForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
+from datetime import datetime
 
 @app.route('/')
 @app.route('/home')
@@ -73,3 +74,9 @@ def user(username):
         {'author': user, 'body' : 'Test post #2'}
     ]
     return render_template('user.html',user=user,posts=posts)
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
