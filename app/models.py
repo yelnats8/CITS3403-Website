9c0e518aa11b8bf170b96c304d686bbb46fcea94
RatKing2002 <nikethkarlapudi75@gmail.com>
2023-05-08 15:20:05 +0800

    2 new fields for database

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
+from datetime import datetime
 
 
 @login.user_loader
@@ -13,6 +14,9 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow())
+    
 
     def __repr__(self):
         return '<User {}>'.format(self.username)    
