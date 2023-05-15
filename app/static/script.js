5bcefe6fa9148c07983b4a2496ca27bb7d6a003f
emifaded <emifadedmusic@gmail.com>
2023-05-15 20:06:33 +0800

    hi

--- app/static/script.js
+++ app/static/script.js
@@ -1,22 +1,25 @@
-// probably need to combine with python files for that
-var isLoggedIn = false; // idk
+// other variables
+var whitebox = document.getElementsByClassName("whitebox");
+var startchatbuttons = document.getElementsByClassName("startchatbtn");
+
+// default
+var userLoggedIn = false;
+// if user is logged on
+if ("disabled" in startchatbuttons.classname == false) {userLoggedIn = true;} 
+// ideally add an event listener to when user logs in or out
 
 // dimensions of window
 var width = window.innerWidth;
 var height = window.innerHeight;
-// other variables
-var whitebox = document.getElementsByClassName("whitebox");
-var buttons = document.getElementsByClassName("startchatbtn");
-
 // window.addEventListener("resize", reportWindowSize);
 window.onresize = function(event) {
     if (width < 700 && height < 1000) {device == "mobile";}
 };
 
-function isUserLoggedIn() {
-    // if user is logged in...
+// debugging bc i cant log on
+function logIn() {
     for (var i = 0; i < buttons.length; i++) {
-        buttons[i].remove("disabled");
+        startchatbuttons[i].remove("disabled");
     }
     return;
 }
