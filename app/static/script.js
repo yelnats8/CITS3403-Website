2a4b3f9ec59048db4e15c22aa0c32cdf7fd53e23
emifaded <emifadedmusic@gmail.com>
2023-05-16 12:23:37 +0800

    yyaaaaay

--- app/static/script.js
+++ app/static/script.js
@@ -5,7 +5,7 @@ var startchatbuttons = document.getElementsByClassName("startchatbtn");
 // default
 var userLoggedIn = false;
 // if user is logged on
-if ("disabled" in startchatbuttons.classname == false) {userLoggedIn = true;} 
+// if ("disabled" in startchatbuttons.classname == false) {userLoggedIn = true;} 
 // ideally add an event listener to when user logs in or out
 
 
@@ -18,5 +18,31 @@ window.onresize = function(event) {
 };
 
 // LOGIN AND REGISTERING
-var requiredfields = document.getElementsByClassName("inputFields");
-var errortext = document.getElementsByClassName("errortext");
+function loadVarLogRegReset() { // set variables
+    var requiredfields = document.getElementsByClassName("inputFields");
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    var confirmpassword = document.getElementById("confirmpassword");
+    var errortext = document.getElementById("errortext");
+}
+
+var form = document.getElementById("registerForm"); // Reference the form element
+
+form.addEventListener('submit', function (event) {
+    checkRegister();
+    loadVarLogRegReset();
+});
+
+function checkRegister() {
+    loadVarLogRegReset();
+    if (username.value == "" || password.value == "") {
+        errortext.innerHTML = "Please enter a username and password.";
+        event.preventDefault(); // Prevent form submission
+    }
+    else if (password.value != "" && confirmpassword.value != "") {
+        if (password.value != confirmpassword.value) {
+            errortext.innerHTML = "Passwords do not match.";
+            event.preventDefault(); // Prevent form submission
+        }
+    }
+}
