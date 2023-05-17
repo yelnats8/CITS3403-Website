ce64ccb3d5221f5a7f140e27edb4413000d4a67b
emifaded <emifadedmusic@gmail.com>
2023-05-17 12:54:34 +0800

    here

---
+++ app/static/script.js
@@ -0,0 +1,38 @@
+
+// load variables
+function loadVar() {
+    // divs
+    var whitebox = document.getElementsByClassName("whitebox");
+    var content = document.getElementsByClassName("content");
+    // buttons
+    var startchatbuttons = document.getElementsByClassName("startchatbtn");
+}
+
+// default
+var userLoggedIn = false;
+// if user is logged on
+// if ("disabled" in startchatbuttons.classname == false) {userLoggedIn = true;} 
+// ideally add an event listener to when user logs in or out
+
+// RESPONSIVITY FOR DEVICE TYPE
+// chatgpt gave me this lol
+var isMobile = false;
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    isMobile = true;
+}
+if (isMobile) {
+    // device is mobile
+} 
+else {
+    // not mobile, keep current css
+}
+
+// and this is my pathetic code
+var width = window.innerWidth;
+var height = window.innerHeight;
+// window.addEventListener("resize", reportWindowSize);
+window.onresize = function(event) {
+    if (width < 700 && height < 1000) {
+        device == "mobile";
+    }
+};
