ec48cf6291af0211aff2607e576afa36248f7f19
RatKing2002 <nikethkarlapudi75@gmail.com>
2023-05-19 22:07:00 +0800

    actual working profiles i swear

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegisterForm, ResetPassForm
+from app.forms import LoginForm, RegisterForm, ResetPassForm, EditProfileForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
 from datetime import datetime
@@ -80,3 +80,18 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
+@app.route('/edit_profile', methods=['GET','POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+       # flash ('Your changes have been saved.')
+        return redirect(url_for('user', username=current_user.username))   #can change to edit_profile so pop up doesnt look ugly of saved changes. or just remove the flash
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',form=form)
