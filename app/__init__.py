0a8097956eaabddb128966ddc8dada74c92ffd03
TechnoStasis <yvesmiguelreyes1@gmail.com>
2023-05-21 21:06:11 +0800

    dynamic directory searching for profile pictures

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_socketio import SocketIO
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['UPLOAD_FOLDER'] = "/Users/RatKing/CITS3403-Website/CITS3403-Website/app/static/avatars"
+app.config['UPLOAD_FOLDER'] = os.getcwd() + "/app/static/avatars"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
