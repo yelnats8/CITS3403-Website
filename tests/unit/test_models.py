433b5d9fd26118e0827d1676b852a67238e138bf
TechnoStasis <yvesmiguelreyes1@gmail.com>
2023-05-21 19:59:15 +0800

    basic tests added

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -3,11 +3,11 @@ from app.models import User
 
 
 def test_new_user():
-    user = User(username= "Farquaad", email = "farquaadmail@gmail.com")
-    user.set_password("princessfiona")
+    user = User(username= "Bowie_on_Fire", email = "davidbowie@gmail.com")
+    user.set_password("catpeople")
 
-    assert user.username == "Farquaad"
-    assert user.email == "farquaadmail@gmail.com"
-    assert user.password_hash != "princessfiona"
-    assert user.check_password("princessfiona") == True
-    assert user.check_password("puss_in_boots") != True
+    assert user.username == "Bowie_on_Fire"
+    assert user.email == "davidbowie@gmail.com"
+    assert user.password_hash != "catpeople"
+    assert user.check_password("catpeople") == True
+    assert user.check_password("loveyoutilltuesday") != True
