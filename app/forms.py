3879c250c29cd3000f902e513883ebb97de300fb
RatKing2002 <nikethkarlapudi75@gmail.com>
2023-05-22 08:34:24 +0800

    added gif support

--- app/forms.py
+++ app/forms.py
@@ -38,5 +38,5 @@ class ResetPassForm(FlaskForm):
 class EditProfileForm(FlaskForm):
     username = StringField ('Username', validators=[DataRequired()])
     about_me = TextAreaField('About me', validators=[Length(min=0,max=140)])
-    avatar = FileField('Avatar', validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+    avatar = FileField('Avatar', validators=[FileAllowed(['jpg', 'png', 'jpeg', 'gif'], 'Images only!')])
     submit = SubmitField('Submit')
